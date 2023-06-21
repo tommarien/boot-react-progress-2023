76cce38323a7b79e4baa9c60de3ce8d169adedca
Tom Marien <tommarien@gmail.com>
2023-06-21 08:02:24 +0200

    chore: added jsonserver

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import useWindowSize from './hooks/useWindowSize';
-
 function App() {
-  const { height, width } = useWindowSize();
   return (
     <>
       <h1>Hello, bootcampers</h1>
-
-      <h2>Window Size</h2>
-
-      <ul>
-        <li>Height: {height}</li>
-        <li>Width: {width}</li>
-      </ul>
     </>
   );
 }
