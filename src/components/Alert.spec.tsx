9d378291982ae557dce0e9176843fd5db76525ff
Tom Marien <tommarien@gmail.com>
2023-06-28 09:06:08 +0200

    chore: up unto dismissible

--- src/components/Alert.spec.tsx
+++ src/components/Alert.spec.tsx
@@ -1,12 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import Alert from './Alert';
+import { ComponentPropsWithoutRef } from 'react';
 
-it('renders an alert', () => {
-  const { rerender } = render(<Alert>My message</Alert>);
+type AlertProps = ComponentPropsWithoutRef<typeof Alert>;
+
+it('renders an alert by default', () => {
+  render(
+    <Alert>
+      <div data-testid="child" />
+    </Alert>,
+  );
 
   const alert = screen.getByRole('alert');
-  screen.debug(alert); // 🖨️ <div role="alert">My Message</div>
+  expect(alert).toHaveClass('alert alert-warning', { exact: true });
 
-  rerender(<Alert>💪</Alert>);
-  screen.debug(alert); // 🖨️ <div role="alert">M💪</div>
+  // it renders children
+  within(alert).getByTestId('child');
+
+  expect(within(alert).queryByRole('heading')).not.toBeInTheDocument();
+});
+
+it.each<AlertProps['variant']>(['info', 'warning', 'danger'])('renders as variant: %s', (variant) => {
+  render(<Alert variant={variant}>My message</Alert>);
+
+  const alert = screen.getByRole('alert');
+  expect(alert).toHaveClass(`alert alert-${variant}`, { exact: true });
+});
+
+it('supports a heading', () => {
+  render(<Alert heading={<div data-testid="headingChild" />}>You have mail</Alert>);
+
+  const alert = screen.getByRole('alert');
+
+  const header = within(alert).getByRole('heading');
+  expect(header).toHaveProperty('tagName', 'H4');
+  expect(header).toHaveClass('alert-heading');
+
+  within(header).getByTestId('headingChild');
+});
+
+it('supports extra classes', () => {
+  render(<Alert className="extra-class">Message from the darkside</Alert>);
+
+  const alert = screen.getByRole('alert');
+  expect(alert).toHaveClass('alert alert-warning extra-class', { exact: true });
 });
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,9 +1,20 @@
+import clsx from 'clsx';
 import { FC, ReactNode } from 'react';
 
 interface AlertProps {
   children: ReactNode;
+  variant?: 'info' | 'warning' | 'danger';
+  heading?: ReactNode;
+  className?: string;
 }
 
-const Alert: FC<AlertProps> = ({ children }) => <div role="alert">{children}</div>;
+const BOOTSTRAP_PREFIX = 'alert';
+
+const Alert: FC<AlertProps> = ({ children, variant = 'warning', heading, className }) => (
+  <div role="alert" className={clsx(BOOTSTRAP_PREFIX, `${BOOTSTRAP_PREFIX}-${variant}`, className)}>
+    {heading && <h4 className={`${BOOTSTRAP_PREFIX}-heading`}>{heading}</h4>}
+    {children}
+  </div>
+);
 
 export default Alert;
