27e7b65ec766fe63f4d3cb2d11bd9f1a8abb1e0f
Tom Marien <tommarien@gmail.com>
2023-07-05 11:26:36 +0200

    refactor: remove async

--- src/main.routes.spec.tsx
+++ src/main.routes.spec.tsx
@@ -4,19 +4,19 @@ import routes from './main.routes';
 
 jest.mock('./pages/Login.tsx', () => () => <div data-testid="fake-login-component" />);
 
-it('renders home by default', async () => {
+it('renders home by default', () => {
   render(<RouterProvider router={createMemoryRouter(routes)} />);
 
   screen.getByText('Home');
 });
 
-it('renders login if path is /login', async () => {
+it('renders login if path is /login', () => {
   render(<RouterProvider router={createMemoryRouter(routes, { initialEntries: ['/login'] })} />);
 
   screen.getByTestId('fake-login-component');
 });
 
-it('renders notFound if path is anything else', async () => {
+it('renders notFound if path is anything else', () => {
   render(<RouterProvider router={createMemoryRouter(routes, { initialEntries: ['/anything-else'] })} />);
 
   screen.getByText('Not Found');
