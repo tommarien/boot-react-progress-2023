a181e1470bd28f5a27b0342e94e642dfc55a3ca9
Tom Marien <tommarien@gmail.com>
2023-07-05 11:09:43 +0200

    chore: completed layout

---
+++ src/main.routes.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import routes from './main.routes';
+
+jest.mock('./pages/Login.tsx', () => () => <div data-testid="fake-login-component" />);
+
+it('renders home by default', async () => {
+  render(<RouterProvider router={createMemoryRouter(routes)} />);
+
+  screen.getByText('Home');
+});
+
+it('renders login if path is /login', async () => {
+  render(<RouterProvider router={createMemoryRouter(routes, { initialEntries: ['/login'] })} />);
+
+  screen.getByTestId('fake-login-component');
+});
+
+it('renders notFound if path is anything else', async () => {
+  render(<RouterProvider router={createMemoryRouter(routes, { initialEntries: ['/anything-else'] })} />);
+
+  screen.getByText('Not Found');
+});
--- src/main.routes.tsx
+++ src/main.routes.tsx
@@ -1,12 +1,27 @@
 import { RouteObject } from 'react-router-dom';
 import Home from './pages/Home';
 import Root from './pages/Root';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <Root />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
   },
 ];
 
